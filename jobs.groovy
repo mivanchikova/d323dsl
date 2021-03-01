014fb7e10f90e13142bd79f9454ae333292b8465
root <root@localhost.localdomain>
2021-03-01 02:52:36 -0500

    changed jobs.groovy

--- jobs.groovy
+++ jobs.groovy
@@ -1,15 +1,15 @@
-job ("MNTLAB-pchekhov-main-build-job") {
+job ("main-build-job") {
  
      parameters {
-         choiceParam('BRANCH_NAME', ['pchekhov', 'master'], 'Branch name')
+         choiceParam('BRANCH_NAME', ['mivanchikova', 'master'], 'Branch name')
          activeChoiceParam('BUILDS_TRIGGER') {
              description('Select job')
              choiceType('CHECKBOX')
              groovyScript {
-                 script('''return ["MNTLAB-pchekhov-child1-build-job",
-                         "MNTLAB-pchekhov-child2-build-job",
-                         "MNTLAB-pchekhov-child3-build-job",
-                         "MNTLAB-pchekhov-child4-build-job"]''')
+                 script('''return ["child1-build-job",
+                         "child2-build-job",
+                         "child3-build-job",
+                         "child4-build-job"]''')
              }
          }
      }
@@ -31,11 +31,11 @@ job ("MNTLAB-pchekhov-main-build-job") {
  }
  
  for (i in (1..4)) {
-        job("MNTLAB-pchekhov-child${i}-build-job") {
+        job("child${i}-build-job") {
              scm {
 				git {
 					remote {
-						url('https://github.com/PavelChekhov/d323dsl.git')
+						url('https://github.com/mivanchikova/d323dsl.git')
 					}
 					branch('$BRANCH_NAME')
 				}
@@ -46,7 +46,7 @@ job ("MNTLAB-pchekhov-main-build-job") {
                     description('Select branch')
 					choiceType('SINGLE_SELECT')
 					groovyScript {
-						script('''def gettags = ("git ls-remote -h https://github.com/PavelChekhov/d323dsl.git").execute()
+						script('''def gettags = ("git ls-remote -h https://github.com/mivanchikova/d323dsl.git").execute()
 return gettags.text.readLines().collect { 
   it.split()[1].replaceAll('refs/heads/', '').replaceAll('refs/tags/', '')}''')
                     }
